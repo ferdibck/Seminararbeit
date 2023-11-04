130014702e4ebe32bc523a8a71369980424183dc
ferdibck <beck.ferdinand@protonmail.com>
2023-11-04 14:22:21 +0100

    .

--- Archiv/test2.py
+++ Archiv/test2.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-p = np.linspace(0, 1, 100)  # Adjust the range as needed
+p = np.linspace(0, 1, 100)
+p_c = 0.59
 
-# Define the critical pressure (p_c)
-p_c = 0.59 # Change this to your desired critical pressure
-
-# Calculate the function value for each p
 y = (p - p_c) ** 1.3
 
-plt.plot(p, y, '-')
+plt.plot(p, y, "-")
 plt.xlabel("p")
 plt.xticks([0, p_c, 1])
 plt.yticks([])
